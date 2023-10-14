e91a8970e5aa85fff055bf0f40274c42cf97d62b
= <=>
2023-10-14 23:58:52 +0900

    feat: 暗黙的な型宣言

--- variables.go
+++ variables.go
@@ -2,12 +2,15 @@ package main
 
 import "fmt"
 
-
 var c, python, java bool
 
 var i, j int = 1,2
 
 func Variables() {
-	var i int
-	fmt.Println(i, c, python, java)
+	var i, j int = 1,2
+	// 暗黙的な型宣言。varを省略出来る。関数内でのみ使用可能。
+	k := 3
+	c, python, java := true, false, "no!"
+	fmt.Println(i, j, k, c, python, java)
+	
 }
